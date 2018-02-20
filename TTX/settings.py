f515e6776d03d69776bb4d7bba77e2933f468f15
mattchang0214 <matthew.chang@tufts.edu>
2018-02-19 20:10:49 -0500

    Updated settings

--- TTX/settings.py
+++ TTX/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'TTX.middleware.LoginRequiredMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'TTX.urls'
