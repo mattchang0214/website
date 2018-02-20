d42e500fdcc66d4b6facbe06f9adb5544c7809a3
mattchang0214 <matthew.chang@tufts.edu>
2018-02-19 22:45:26 -0500

    Edited settings

--- TTX/wsgi.py
+++ TTX/wsgi.py
@@ -15,6 +15,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTX.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-from dj_static import Cling
-application = Cling(get_wsgi_application())
