445b5f4e9fbd6cdaa4ae6f540878470b087a8a92
mattchang0214 <matthew.chang@tufts.edu>
2018-02-19 21:46:15 -0500

    Idk

--- TTX/settings.py
+++ TTX/settings.py
@@ -29,7 +29,7 @@ DATABASES = {
     )
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,8 +136,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/accounts/login/'
 
